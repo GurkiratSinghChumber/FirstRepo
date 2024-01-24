8fcdf7a903c921951ea0d87552d75a86939d4706
Gurkirat Singh Chumber <chumbgur@sheridancollege.ca>
2024-01-24 10:20:59 -0500

    added a part time student class

---
+++ src/helloworld/PartTimeStudent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author gur28
+ */
+public class PartTimeStudent {
+    private int numOfCourses;
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
